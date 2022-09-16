4abe03517f4e5223c96bd753c40aa63986ed5929
Guilherme Gomes <35308442+Guilherme09396@users.noreply.github.com>
2022-09-16 17:27:07 -0300

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 mongoose.Promise = global.Promise;
 
-mongoose.connect("mongodb+srv://guilherme09396:guilherme9812@cluster0.4k7p4vl.mongodb.net/?retryWrites=true&w=majority", {
+mongoose.connect("mongodb+srv://guilherme09396:<password>@cluster0.4k7p4vl.mongodb.net/?retryWrites=true&w=majority", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 }).then(() => {
